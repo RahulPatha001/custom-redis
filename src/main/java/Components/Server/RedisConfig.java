9b556df66eec33d1fe5c8bc13f0619962a5825ab
RahulPatha001 <rahul.patha29@gmail.com>
2025-07-20 17:56:26 +0530

    ci: added initial replication id and offset

--- src/main/java/Components/Server/RedisConfig.java
+++ src/main/java/Components/Server/RedisConfig.java
@@ -2,12 +2,41 @@ package Components.Server;
 
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class RedisConfig {
     private String role;
     private int port;
     private String masterHost;
     private int masterPort;
+    private String masterReplId = null;
+    private Long masterReplOffset = null;
+
+    public Long getMasterReplOffset() {
+        if(masterReplOffset == null){
+            masterReplOffset = 0L;
+        }
+        return masterReplOffset;
+    }
+
+    public void setMasterReplOffset(Long masterReplOffset) {
+        this.masterReplOffset = masterReplOffset;
+    }
+
+    public String getMasterReplId() {
+        if(masterReplId == null){
+            masterReplId = UUID.randomUUID().toString().replace("-","") +
+                    UUID.randomUUID().toString().replace("-","").substring(0,8);
+
+        }
+        return masterReplId;
+    }
+
+    public void setMasterReplId(String masterReplId) {
+        this.masterReplId = masterReplId;
+    }
+
     public String getMasterHost() {
         return masterHost;
     }
